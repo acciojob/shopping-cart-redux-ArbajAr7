fa28559bb7479d4291131b739890e7f547346af5
ArbajAr7 <arbajrmacr786@gmail.com>
2024-08-02 20:42:09 +0530

    fourth commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import Store from "./components/store";
+import store from './store'
+import App from './App'
+import './index.css'
 
-
-ReactDOM.render(<Provider store={Store}><App /></Provider>
-, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
